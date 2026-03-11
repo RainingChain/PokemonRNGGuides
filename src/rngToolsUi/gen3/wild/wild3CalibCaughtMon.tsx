27a880cf6d056a71cab15819bb043ea069933639
RainingChain <rainingchain@gmail.com>
2026-03-10 20:59:42 -0400

    .

--- src/rngToolsUi/gen3/wild/wild3CalibCaughtMon.tsx
+++ src/rngToolsUi/gen3/wild/wild3CalibCaughtMon.tsx
@@ -18,16 +18,25 @@ import { toOptions } from "~/utils/options";
 import { natureOptions } from "~/components/pkmFilter";
 import { getStatFields } from "~/rngToolsUi/shared/statFields";
 import { defaultMinMaxStats, StatFieldsSchema } from "~/types";
-import { Gen3Method, Species, Wild3EncounterGameData } from "~/rngTools";
+import {
+  Gen3Method,
+  Species,
+  SpeciesData,
+  Wild3Action,
+  Wild3EncounterGameData,
+} from "~/rngTools";
 import { getWild3EmeraldGameData } from "./data/wild3GameData";
 import type { FormState as TargetSetup } from "./wild3CalibTarget";
+import { isFishingAction, wild3Actions } from "./utils";
+import { useWatch } from "react-hook-form";
+import { GenderFilter } from "~/components/genderFilter";
 
 const emeraldWildGameData = getWild3EmeraldGameData();
 
 const Validator = z
   .object({
     nature: z.enum(nature),
-    gender: z.enum(["Male", "Female"]),
+    filter_gender: z.enum(["Male", "Female"]),
     species: z.enum(emeraldWildGameData.species),
     lvl: z.number().min(1).max(100),
     //NO_PROD ability
@@ -44,7 +53,7 @@ const initialValues: FormState = {
   spdStat: 0,
   speStat: 0,
   nature: "Adamant",
-  gender: "Male",
+  filter_gender: "Male", // Must be named filter_gender for GenderFilter component
   species: "Shuckle",
   lvl: 1,
 };
@@ -60,11 +69,72 @@ export type CaughtMonResult = {
   method: Gen3Method;
 };
 
-const Fields = () => {
-  //NO_PROD species first. if gender if not genderless etc.
-  const speciesList = []; // based on targetSetup
-  const fields = React.useMemo(
-    () => [
+const getPossibleEncountersForMap = (targetSetup: TargetSetup) => {
+  const map_data = emeraldWildGameData.maps_data.find(
+    (map) => map.map_id === targetSetup.map,
+  );
+  if (map_data == null) {
+    return [];
+  }
+
+  const list: Wild3EncounterGameData[] = [
+    ...map_data.slots_by_action[wild3Actions.indexOf(targetSetup.action)],
+  ];
+  if (isFishingAction(targetSetup.action) && map_data.feebas != null) {
+    list.push(map_data.feebas);
+  }
+  if (targetSetup.action === "SweetScentLand") {
+    list.push(
+      ...map_data.roamers
+        .filter((roamer) => roamer.id === targetSetup.roamerState)
+        .map((roamer) => roamer.encounter_data),
+    );
+  }
+  if (
+    targetSetup.action === "SweetScentLand" ||
+    targetSetup.action === "SweetScentWater"
+  ) {
+    list.push(
+      ...map_data.mass_outbreaks
+        .filter(
+          (mass_outbreak) => mass_outbreak.id === targetSetup.massOutbreakState,
+        )
+        .map((mass_outbreak) => mass_outbreak.encounter_data),
+    );
+  }
+  return list;
+};
+
+const Fields = ({ targetSetup }: { targetSetup: TargetSetup }) => {
+  const selectedSpecies = useWatch<FormState, "species">({ name: "species" });
+
+  const fields = React.useMemo(() => {
+    const encounters = getPossibleEncountersForMap(targetSetup);
+    const speciesList = encounters.map((enc) => enc.species_data.species);
+    const selectedSpeciesInfos = encounters.filter(
+      (enc) => enc.species_data.species === selectedSpecies,
+    );
+
+    const speciesField: Field = {
+      label: "Species",
+      input: (
+        <FormikRadio<FormState>
+          name="species"
+          options={toOptions(speciesList)}
+        />
+      ),
+    };
+
+    if (selectedSpeciesInfos.length === 0) {
+      return [];
+    }
+
+    const genderRatio = selectedSpeciesInfos?.[0].species_data.gender_ratio;
+    const minMaxLvl = Math.min(
+      ...selectedSpeciesInfos.map((info) => info.min_level),
+    );
+
+    return [
       {
         label: "Species",
         input: (
@@ -73,26 +143,20 @@ const Fields = () => {
             options={toOptions(speciesList)}
           />
         ),
-        indent: 1,
       },
-      /* { //TODO
+      {
+        //TODO
         label: "Level",
         input: (
           <FormikRadio<FormState>
-            name="gender"
+            name="lvl"
             options={toOptions(["Male", "Female"] as const)}
           />
         ),
-      },*/
+      },
       {
         label: "Gender",
-        input: (
-          <FormikRadio<FormState>
-            name="gender"
-            options={toOptions(["Male", "Female"] as const)}
-          />
-        ),
-        indent: 1,
+        input: <GenderFilter genderRatio={genderRatio} />,
       },
       {
         label: "Nature",
@@ -102,12 +166,10 @@ const Fields = () => {
             options={natureOptions.required}
           />
         ),
-        indent: 1,
       },
       ...getStatFields<FormState>(defaultMinMaxStats), //NO_PROD init defaultMinMaxStats with species
-    ],
-    [],
-  );
+    ];
+  }, [targetSetup]);
 
   return <FormFieldTable fields={fields} />;
 };
@@ -206,7 +268,9 @@ export const Wild3CalibCaughtMon = ({
         submitButtonLabel="Find advances matching caught Pokémon"
         rowKey="advance"
       >
-        <Fields />
+        <div style={{ paddingLeft: "20px" }}>
+          <Fields />
+        </div>
       </RngToolForm>
     </Flex>
   );
